7b3b40ee550f1ff8aa28b6811d54e67b7eab6c2c
Sansith <seniyadissanayake@gmail.com>
2024-12-11 18:30:04 +0530

    feat: create shared components

---
+++ models/src/lib/eventup-models.ts
@@ -0,0 +1,41 @@
+export interface Participant {
+  employee_name: string;
+  email: string;
+  first_name: string;
+  ref_id: string;
+  smsLogs: { number: string; smsSent: 'Y' | 'N' }[];
+  qrUrl: string;
+  checkIns: {
+    checkpointCode: string;
+    isChecked: boolean;
+    checkedInTime: string;
+  }[];
+  votes: {
+    contestantId: string;
+    timestamp: string;
+  }[];
+}
+
+export interface Contestant {
+  name: string;
+  photoUrl: string;
+  id: string;
+  voteCount: number;
+}
+
+export interface DecodeResponse {
+  data: {
+    beverages?: string[];
+    email: string;
+    emp_no?: string;
+    food_preference?: string;
+    id?: number;
+    name: string;
+    nic?: string;
+    vehicle_no?: string;
+  };
+}
+
+export interface SystemConfig {
+  values: { code: string; name: string }[];
+}
